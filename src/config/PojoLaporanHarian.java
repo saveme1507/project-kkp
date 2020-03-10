f1b68fadabee52ebf2a89dee024a79e53501d35d
saveme1507 <asepsoo55@gmail.com>
2020-03-10 21:01:11 +0700

    menambah class pojo lap harian

--- src/config/PojoLaporanHarian.java
+++ src/config/PojoLaporanHarian.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package config;
 
-/**
- *
- * @author saveme
- */
+
 public class PojoLaporanHarian {
+
+    private int kode,stokAwal,Harga,terjual,jumlah;
+    private String produk;
+
+    public int getKode() {
+        return kode;
+    }
+
+    public void setKode(int kode) {
+        this.kode = kode;
+    }
+
+    public int getStokAwal() {
+        return stokAwal;
+    }
+
+    public void setStokAwal(int stokAwal) {
+        this.stokAwal = stokAwal;
+    }
+
+    public int getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(int Harga) {
+        this.Harga = Harga;
+    }
+
+    public int getTerjual() {
+        return terjual;
+    }
+
+    public void setTerjual(int terjual) {
+        this.terjual = terjual;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public String getProduk() {
+        return produk;
+    }
+
+    public void setProduk(String produk) {
+        this.produk = produk;
+    }
     
 }
